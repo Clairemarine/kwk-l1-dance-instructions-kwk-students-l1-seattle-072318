a3f22df4494c14931b57c0c1fb616b420a01c975
Claire Nguyen <pandascn@gmail.com>
2018-07-24 16:35:10 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -3,4 +3,46 @@
 
 def gangnam_style
   
-  
+  Assume the proper stance
+
+1. Plant legs far apart, bend knees slightly and keep posture loose
+
+Base Footwork
+
+1. Lift right foot
+
+2. Return right foot to the ground
+
+3. Finishing with a **small skip-step** backward
+
+
+
+### **Skip-Step**
+
+Skip Step Left
+
+1. Lower left foot to the ground
+2. Bounce left foot back up slightly, kicking it a few inches back
+3. Left, Right, Left and Left
+
+Skip Step Right
+
+1. Lower right foot to the ground
+2. Bounce right foot back up slightly, kicking it a few inches back
+3. Right, Left, Right and Right
+
+
+
+### Upper Body
+
+##### Bob the reins
+
+1. Holding your arms out in front of you, straight and at chest level
+2. Cross your right wrist over your left and hold them together
+3. Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated **eight** times.
+
+**Lasso**
+
+1. Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left.
+   - Lift right arm up and point right elbow diagonally to the right.
+   - Raise right forearm so that it points straight up, and **whip it in small circles** to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated **eight** times as well.
