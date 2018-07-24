fa4c8b5496f2c6b3be9abad6bb63106035346e44
Claire Nguyen <pandascn@gmail.com>
2018-07-24 16:39:06 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -2,10 +2,24 @@
 # Code your methods below
 
 def lower_body
-  
+ Assume the proper stance
+
+1. Plant legs far apart, bend knees slightly and keep posture loose
+
+Base Footwork
+
+1. Lift right foot
+
+2. Return right foot to the ground
+
+3. Finishing with a **small skip-step** backward 
   
 
 end
+
+def skip_step
+  
+  
 
 
 
