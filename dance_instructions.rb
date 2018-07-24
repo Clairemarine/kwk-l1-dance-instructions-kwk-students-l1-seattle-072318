643dcf12e18d3e93c562564a9a4f393b27542471
Claire Nguyen <pandascn@gmail.com>
2018-07-24 16:45:36 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -5,46 +5,46 @@ def lower_body
   
 puts Assume the proper stance
 
-1. Plant legs far apart, bend knees slightly and keep posture loose
+puts 1. Plant legs far apart, bend knees slightly and keep posture loose
 
-Base Footwork
+puts Base Footwork
 
-1. Lift right foot
+puts 1. Lift right foot
 
-2. Return right foot to the ground
+puts 2. Return right foot to the ground
 
-3. Finishing with a **small skip-step** backward 
+puts 3. Finishing with a **small skip-step** backward 
   
 
 end
 
 def skip_step
   
-  Skip Step Left
+puts  Skip Step Left
 
-1. Lower left foot to the ground
-2. Bounce left foot back up slightly, kicking it a few inches back
-3. Left, Right, Left and Left
+puts 1. Lower left foot to the ground
+puts 2. Bounce left foot back up slightly, kicking it a few puts inches back
+puts 3. Left, Right, Left and Left
 
-Skip Step Right
+puts Skip Step Right
 
-1. Lower right foot to the ground
-2. Bounce right foot back up slightly, kicking it a few inches back
-3. Right, Left, Right and Right
+puts 1. Lower right foot to the ground
+puts 2. Bounce right foot back up slightly, kicking it a few puts inches back
+puts 3. Right, Left, Right and Right
 
 end 
 
 def upper_body
   
-  1. Holding your arms out in front of you, straight and at chest level
-2. Cross your right wrist over your left and hold them together
-3. Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated **eight** times.
+  puts "1. Holding your arms out in front of you, straight and  at chest level
+s 2. Cross your right wrist over your left and hold them  together
+ 3. Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated **eight** times.
 
-**Lasso**
+ **Lasso**
 
-1. Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left.
-   - Lift right arm up and point right elbow diagonally to the right.
-   - Raise right forearm so that it points straight up, and **whip it in small circles** to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated **eight** times as well.
+ 1. Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left.
+    - Lift right arm up and point right elbow diagonally to the right.
+   - Raise right forearm so that it points straight up, and **whip it in small circles** to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated **eight** times as well."
    
  end
   
